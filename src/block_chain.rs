706cccb650f81dd024de5fad89ca4343969fc8b7
flora <flora_chen2021@163.com>
2025-03-05 17:19:54 +0800

    update miner_block func

--- src/block_chain.rs
+++ src/block_chain.rs
@@ -1,4 +1,5 @@
 use crate::hash_function::{calculate_merkle_root, hash_block_header};
+use crate::serialization::serialize_bc;
 use crate::transaction::Transaction;
 use chrono::Utc;
 use rand::Rng;
@@ -20,17 +21,12 @@ pub struct Block {
     pub transactions: Vec<Transaction>, // 交易列表
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct BlockChain {
-    pub blocks: Vec<Block>, // 区块列表
-    pub transaction_pool: Vec<Transaction>,
-}
-
 impl Block {
     // 创建一个新的区块
     pub fn new() -> Block {
         // 生成随机数 nonce
         let nonce = rand::rng().random();
+        // let nonce = 0;
         // 生成当前时间戳
         let timestamp = Utc::now().timestamp() as u32;
         Block {
@@ -47,6 +43,12 @@ impl Block {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BlockChain {
+    pub blocks: Vec<Block>, // 区块列表
+    pub transaction_pool: Vec<Transaction>,
+}
+
 impl BlockChain {
     // 创建一个新的区块链
     pub fn new() -> Self {
@@ -61,52 +63,23 @@ impl BlockChain {
         self.transaction_pool.push(transaction);
     }
 
-    // 挖矿：打包交易并生成新区块
-    pub fn mine_block(&mut self, miner_address: String) {
-        let previous_block = self.blocks.last().unwrap();
-        let previous_hash = previous_block.header.prev_block_hash.clone();
-        let mut new_block = Block {
-            header: BlockHeader {
-                version: previous_block.header.version + 1,
-                prev_block_hash: previous_hash,
-                merkle_root: calculate_merkle_root(&self.transaction_pool),
-                timestamp: Utc::now().timestamp() as u32,
-                bits: 0,
-                nonce: 0,
-            },
-            transactions: self.transaction_pool.clone(),
-        };
-
-        // 工作量证明（PoW）
-
-        // 将新区块添加到区块链
-        self.add_block(new_block);
-
+    pub fn mine_block(&mut self, difficulty: usize) {
+        let mut new_block = Block::new();
+        // 将交易池中的交易添加到新区块的交易列表
+        new_block.transactions = self.transaction_pool.clone();
         // 清空交易池
         self.transaction_pool.clear();
-
-        // 奖励矿工
-        // let reward_transaction = Transaction {
-        //     sender: String::from("0"), // 系统奖励
-        //     receiver: miner_address,
-        //     amount: 50, // 挖矿奖励
-        // };
-        // self.add_transaction(reward_transaction);
+        // 计算新区块的 Merkle Root（假设交易列表已经设置）
+        new_block.header.merkle_root = calculate_merkle_root(&new_block.transactions);
+        // 计算新区块的哈希值
+        let mut hash = hash_block_header(&new_block.header);
+        while !hash.starts_with(&[0; 32][..difficulty]) {
+            new_block.header.nonce += 1;
+            hash = hash_block_header(&new_block.header);
+        }
+        // 将新区块添加到区块链
+        self.add_block(new_block);
     }
-
-    // // 工作量证明算法
-    // fn proof_of_work(&self, block: &mut Block) -> String {
-    //     let difficulty = 4; // 难度值，要求哈希值前 4 位为 0
-    //     loop {
-    //         let block_data = serde_json::to_string(&block).unwrap();
-    //         let hash = sha256_hash(&block_data);
-    //         if hash.starts_with(&"0".repeat(difficulty)) {
-    //             return hash;
-    //         }
-    //         block.nonce += 1;
-    //     }
-    // }
-
     pub fn add_block(&mut self, data: Block) {
         let mut new_block = data;
         if let Some(last_block) = self.blocks.last() {
@@ -121,6 +94,8 @@ impl BlockChain {
 
         // 计算新区块的 Merkle Root（假设交易列表已经设置）
         new_block.header.merkle_root = calculate_merkle_root(&new_block.transactions);
+
+        // new_block.mine_block(4); // 假设难度是4
 
         // 将新区块添加到区块链
         self.blocks.push(new_block);
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use ::block_chain::block_chain::Block;
 use ::block_chain::serialization;
+use ::block_chain::transaction;
 use block_chain::block_chain;
 
 fn main() {
-    let _block = block_chain::Block::new();
-    let blockchain = block_chain::BlockChain::new();
-    let bytes = serialization::serialize_bc(&blockchain).unwrap();
-    let blockchain2: block_chain::BlockChain = serialization::deserialize_bc(&bytes).unwrap();
-    println!("{:?}", blockchain2);
-    println!("Hello, world!");
+    // 0. 创世区块
+    let mut blockchain = block_chain::BlockChain::new();
+    // 1. 新建交易：Alice声称转账给Bob 10个比特币
+    let tx1 = transaction::Transaction::new(
+        1,
+        vec![transaction::TxIn::new([0; 32], vec![], 0)],
+        vec![transaction::TxOut::new(10, vec![1; 32])],
+        0,
+    );
+    // 2. 将交易添加到交易池
+    blockchain.add_transaction(tx1);
+    // 3. 挖矿并将节点加入账本
+    blockchain.mine_block(4);
+    // 4. 新建交易：Bob声称转账给Alice 5个比特币
 }
